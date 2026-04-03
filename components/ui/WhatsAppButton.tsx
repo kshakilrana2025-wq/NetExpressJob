cc6c919ee8ffdfa96bde020103d4526fc7689a0c
kshakilrana2025-wq <kshakilrana2025@gmail.com>
2026-04-03 12:30:28 +0600

    Create WhatsAppButton.tsx

---
+++ components/ui/WhatsAppButton.tsx
@@ -0,0 +1,16 @@
+export default function WhatsAppButton() {
+  return (
+    <a
+      href="https://wa.me/8801890193958"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-6 right-6 z-50"
+    >
+      <img
+        src="https://i.postimg.cc/C1j0H25x/Chat-GPT-Image-Sep-20-2025-07-06-41-PM.png"
+        alt="WhatsApp"
+        className="w-14 h-14 rounded-full shadow-lg hover:scale-110 transition"
+      />
+    </a>
+  );
+}
