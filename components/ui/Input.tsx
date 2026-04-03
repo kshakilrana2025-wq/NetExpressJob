d13dab1fed1c3cce8ca2c632b63bb90a971b3803
kshakilrana2025-wq <kshakilrana2025@gmail.com>
2026-04-03 12:34:37 +0600

    Update Input.tsx

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,5 +1,6 @@
 import { InputHTMLAttributes } from 'react';
 
+
 export default function Input(props: InputHTMLAttributes<HTMLInputElement>) {
   return <input {...props} className={`w-full border rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-500 ${props.className || ''}`} />;
 }
