ed433638c81d105b4e4d2df1bf3f146f2999165b
kshakilrana2025-wq <kshakilrana2025@gmail.com>
2026-04-03 12:33:51 +0600

    Update Card.tsx

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 
+
 export default function Card({ children, className = '' }: { children: ReactNode; className?: string }) {
   return <div className={`bg-white rounded-xl shadow-md p-6 ${className}`}>{children}</div>;
 }
